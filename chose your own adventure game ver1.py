351d8084209abfb112465d967b2cffd4b33baa6a
maxatesa <101612344+maxatesa@users.noreply.github.com>
2022-03-17 12:24:27 +1300

    added version 1
    pre-beta for testing paths, current choices are temps

---
+++ chose your own adventure game ver1.py
@@ -0,0 +1,15 @@
+a = 0
+b = 0
+c = 0
+d = 0
+e = 0
+input_a = input("lore ipsum path left or right ")
+if input_a == "left":
+    print("path_left test ")
+if input_a == "right":
+    print("path_right ")
+    input_a1 = input("balls ")
+    if input_a1 == "cum":
+        print("cum ")
+    if input_a1 == "shit":
+        print("shit ")
